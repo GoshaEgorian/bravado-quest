423e5fcc0cc424a8c042d995237560f7fd73a1be
George Egorian <gosha.egorian@gmail.com>
2020-02-16 05:13:49 +0300

    Add vue router

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,53 +1,18 @@
 import Vue from '../../node_modules/vue/dist/vue.esm';
+import VueRouter from '../../node_modules/vue-router/dist/vue-router.esm';
 
-import virtualList from '../../node_modules/vue-virtual-scroll-list/dist/index';
 import highlight from 'vue-highlight-text/public/directive.min.js';
 
-import Person from './blocks/Person';
-import SearchInput from './blocks/SearchInput';
+import Container from './blocks/Container';
 
-import get from './helpers/get';
+Vue.use(VueRouter)
 
 Vue.directive('highlight', highlight);
 
-export default () => new Vue({
-  el: '#app',
-  data: {
-    users: [],
-    keyword: '',
-    size: 136 + 21,
-  },
-  computed: {
-    remain: function(){
-      const freeHeight = window.innerHeight - (50 + 12 * 2);
-      return Math.floor(freeHeight / this.size);
-    },
-    filtered: function(){
-      const {users, keyword} = this;
+const routes = [
+  {path: '/', name: '/', component: Container},
+];
 
-      if (keyword === '') return users;
+const router = new VueRouter({routes});
 
-      return  users.filter(({
-        name,
-        email,
-        title,
-        city,
-        address,
-      }) =>
-        name.includes(keyword) ||
-        email.includes(keyword) ||
-        title.includes(keyword) ||
-        city.includes(keyword) ||
-        address.includes(keyword)
-      );
-    },
-  },
-  created: function(){
-    get('/users').then(res => (this.users = res));
-  },
-  components: {
-    'person': Person,
-    'search-input': SearchInput,
-    'virtual-list': virtualList,
-  },
-});
+export default () => new Vue({router}).$mount('#app');
